e34a13b4c15fb460237edcbbd147311bd11ffede
Motyak <m0tyak_@live.fr>
2025-06-17 17:29:34 +0200

    some fixes

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 ./build.sh
 
-ln -s "$(realpath monlang.sh)" ~/.local/bin/monlang
-ln -s "$(realpath monlang-server/monlang_server.sh)" ~/.local/bin/monlang_server
-ln -s "$(realpath monlang-server/open_monlang.sh)" ~/.local/bin/open_monlang
+ln -fs "$(realpath monlang.sh)" ~/.local/bin/monlang
+ln -fs "$(realpath monlang-server/monlang_server.sh)" ~/.local/bin/monlang_server
+ln -fs "$(realpath monlang-server/open_monlang.sh)" ~/.local/bin/open_monlang
--- monlang.sh
+++ monlang.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -o errexit
 
+function die {
+    >&2 echo $@
+    exit 1
+}
+
+[ "$1" != "" ] || die "Expected a file argument"
+[ -f "$1" ] || die "Cannot find file \`$1\`"
+
 monlang-parser/bin/main.elf\ -o $@
 monlang-interpreter/bin/main.elf $@
