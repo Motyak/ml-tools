b0fe87be675148aa6ffc4cae55d981fa2717e190
Motyak <m0tyak_@live.fr>
2025-06-19 00:22:02 +0200

    now monlang.sh support all modes GÉNIE

--- monlang.sh
+++ monlang.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 set -o errexit
 
-function die {
-    >&2 echo $@
-    exit 1
-}
+trap 'rm -rf "$TMPDIR"; jobs -p | xargs -r kill' EXIT
 
-[ "$1" != "" ] || die "Expected a file argument"
-[ -f "$1" ] || die "Cannot find file \`$1\`"
+# REPL mode #
+if [ "$1" == "" ]; then
+    TMPDIR="$(mktemp -d)"
+    TMPFILE="$(mktemp -p "$TMPDIR")"
+    PIPE1="$(mktemp -u -p "$TMPDIR")"
+    PIPE2="$(mktemp -u -p "$TMPDIR")"
+    mkfifo "$PIPE1" "$PIPE2"
+    monlang-parser/bin/main.elf\ -o < "$PIPE1" &
+    monlang-interpreter/bin/main.elf < "$PIPE2" &
+    while true; do
+        >/dev/null tee "$TMPFILE"
+        cat "$TMPFILE" > "$PIPE2"
+        cat "$TMPFILE" > "$PIPE1"
+    done
 
-monlang-parser/bin/main.elf\ -o $@
-monlang-interpreter/bin/main.elf $@
+# stdin mode #
+elif [ "$1" == "-" ]; then
+    TMPDIR="$(mktemp -d)"
+    TMPFILE="$(mktemp -p "$TMPDIR")"
+    >/dev/null tee "$TMPFILE"
+    monlang-parser/bin/main.elf\ -o - < "$TMPFILE"
+    monlang-interpreter/bin/main.elf - < "$TMPFILE"
+
+# filein mode #
+else
+    monlang-parser/bin/main.elf\ -o "$1"
+    monlang-interpreter/bin/main.elf "$1"
+fi
+
+
